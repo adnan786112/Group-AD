62bf42024755fd117bc577a0838c28f91b59f65f
ec251035 <ec251035@qmul.ac.uk>
2025-10-27 20:12:05 +0000

    added heuristics

--- src/main/java/core/Game.java
+++ src/main/java/core/Game.java
@@ -14,6 +14,7 @@ import gui.AbstractGUIManager;
 import gui.GUI;
 import gui.GamePanel;
 import players.basicMCTS.BasicMCTSPlayer;
+import players.basicMCTS.groupAD.GroupADBasicMCTSPlayer;
 import players.human.ActionController;
 import players.human.HumanConsolePlayer;
 import players.human.HumanGUIPlayer;
@@ -706,10 +707,11 @@ public class Game {
         ArrayList<AbstractPlayer> players = new ArrayList<>();
         //players.add(new RandomPlayer());
         //players.add(new RandomPlayer());
+        players.add(new GroupADBasicMCTSPlayer());
         players.add(new BasicMCTSPlayer());
        //players.add(new OSLAPlayer());
 //        players.add(new RMHCPlayer());
-        players.add(new HumanGUIPlayer(ac));
+        // players.add(new HumanGUIPlayer(ac));
 
 
         /* Game parameter configuration. Set to null to ignore and use default parameters */
